248ab43aa62701eed9d904c1c187f6cf2f5bdeb2
Tim Smith <tsmith84@gmail.com>
2015-11-05 11:21:19 -0800

    Delete all content and update the readme

--- roles/django_cms.rb
+++
@@ -1,7 +0,0 @@
-name "django_cms"
-description "django_cms front end application server."
-run_list(
-  "recipe[mysql::client]",
-  "recipe[application]",
-  "recipe[django_cms::status]"
-)
--- roles/django_cms_database_master.rb
+++
@@ -1,5 +0,0 @@
-name "django_cms_database_master"
-description "database master for the django_cms application."
-run_list(
-  "recipe[database::master]"
-)
